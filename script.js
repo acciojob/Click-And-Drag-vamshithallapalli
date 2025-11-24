e861ce2f38a9bb75d002bd037fb171f76b1eaa3d
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-11-24 08:26:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 // Your code here.
+// Your code goes here
+
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  
+  // Mouse initial X relative to the slider
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  
+  const x = e.pageX - slider.offsetLeft;
+  
+  const walk = (x - startX) * 2;  // faster scroll
+  slider.scrollLeft = scrollLeft - walk;
+});
